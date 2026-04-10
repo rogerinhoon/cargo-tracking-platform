be7dc598dd293306cc7d065665e2038fb4363eca
Rogério <rogerio@piloti.com.br>
2026-04-10 16:01:02 -0300

    feat: add projeto

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\SobreController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/sobre', SobreController::class);
