829cc5cb01d55d81280f70631ac6add7b7e6ea49
Rogério <rogerio@piloti.com.br>
2026-04-10 18:12:44 -0300

    feat: page rastreamento

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RatreamentoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
-
+Route::get('/rastreamento', RatreamentoController::class);
